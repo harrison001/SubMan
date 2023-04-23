6b10802b2c51937341f0d2bdc60f190aa6c16d6a
Harrison Guo <gzxharrison@gmail.com>
2023-04-22 18:30:02 -0700

    build production envrionment

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-nohup python main.py > subman.log 2>&1 &
+#!/bin/bash
+
+# 查找并结束之前创建的进程
+pid=$(ps -ef | grep "subman.py" | grep -v grep | awk '{print $2}')
+if [ -n "$pid" ]; then
+    echo "Stopping previous process with PID $pid"
+    kill $pid
+fi
+
+# 启动新的后台进程
+nohup python subman.py > subman.log 2>&1 &
+echo "New process started"
