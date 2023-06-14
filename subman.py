74d7feaac79748a202b91469845da32ed05620d8
Harrison Guo <gzxharrison@gmail.com>
2023-06-14 08:26:49 -0700

    improvment for log by adding datetime

--- subman.py
+++ subman.py
@@ -34,7 +34,9 @@ MONGODB_DB_NAME = os.environ.get('MONGODB_DB_NAME')
 WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
 
 
-logging.basicConfig(filename='subman.log', level=logging.INFO)
+logging.basicConfig(filename='subman.log', level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 
